30c3846e6750ebd231bfe041d80124852f6cf377
David Wilhelm <dave@jumbledpile.com>
2013-08-09 13:10:34 -0700

    add a PowerShell profile
    --HG--
    branch : win

--- install.bat
+++ install.bat
@@ -1,5 +1,15 @@
 @echo off
 
+setlocal
+
 for %%i in (mercurial.ini .hgignore .vimrc .gitconfig .gitignore) do @(
 	copy /Y %%i "%USERPROFILE%"
 )
+
+set PSPROFILEDIR=%USERPROFILE%\My Documents\WindowsPowerShell
+if not exist "%PSPROFILEDIR%" (
+    mkdir "%PSPROFILEDIR%"
+)
+copy /Y Microsoft.PowerShell_profile.ps1 "%PSPROFILEDIR%"
+
+endlocal
