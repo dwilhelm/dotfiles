869669e1fa1a1b86133cdc01ef6b73c6d7261596
David Wilhelm <dave@jumbledpile.com>
2025-07-03 02:43:35 -0700

    sync: Refactor, more pythonic with same functionality

--- home/bin/syncwith.py
+++ home/bin/syncwith.py
@@ -4,16 +4,16 @@ import argparse
 import os
 import socket
 import subprocess
-import sys
 
 syncpaths = ["Documents", "Pictures", "edu"]
-parser = argparse.ArgumentParser(prog="syncwith")
-parser.add_argument('-n', '--dry-run', action='store_true')
-parser.add_argument('--delete', action='store_true')
-parser.add_argument('remote_csv_list')
-args = parser.parse_args(sys.argv[1:])
+def parse_args():
+    parser = argparse.ArgumentParser(prog="syncwith")
+    parser.add_argument('-n', '--dry-run', action='store_true')
+    parser.add_argument('--delete', action='store_true')
+    parser.add_argument('remote_csv_list')
+    return parser.parse_args()
 
-def syncdir(remotebase, path):
+def syncdir(remotebase, path, args):
     rsyncopts = "-rlptuvz"
     rsynclongopts = ["--exclude", ".*.swp", "--exclude", ".DS_Store", "--exclude", "Photos Library.photoslibrary"]
     localpath = f"{os.environ['HOME']}/{path}/"
@@ -29,8 +29,13 @@ def syncdir(remotebase, path):
     print(f"\nSync {remotepath} to {localpath}\n{rsynccmd}")
     subprocess.run(rsynccmd + [remotepath, localpath])
 
-for remote in args.remote_csv_list.split(','):
-    if remote == f"{os.environ['USER']}@{socket.gethostname()}":
-        continue
-    for syncpath in syncpaths:
-        syncdir(f"{remote}:", syncpath)
+def main():
+    args = parse_args()
+    for remote in args.remote_csv_list.split(','):
+        if remote == f"{os.environ['USER']}@{socket.gethostname()}":
+            continue
+        for syncpath in syncpaths:
+            syncdir(f"{remote}:", syncpath, args)
+
+if __name__ == '__main__':
+    main()
